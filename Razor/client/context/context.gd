9ccc1e8e1e94fd719e16a3b81e391ae9a4c00cb4
Josué Fabricio Urbina González <urbgon@gmail.com>
2021-12-24 20:08:21 -0600

    Add basic proto network

--- Razor/client/context/context.gd
+++ Razor/client/context/context.gd
@@ -1,6 +1,16 @@
+extends Node
 
 class_name Context
 
+const MoonProto = preload("res://client/context/moon_proto.gd")
+const Network = preload("res://client/network/net_v2.gd")
+
+var netHandler = null
+var player_data = {}
+
+func _ready():
+	netHandler = Network.new()
+	
 func initContext():
 	# network
 	# - security
@@ -9,5 +19,35 @@ func initContext():
 
 func ini_multiplayer():
 	# network
-	
 	pass
+	
+func encode_player_data():
+	var data = MoonProto.PlayerInfo.new()
+	var type = MoonProto.PlayerInfo.TypeNode.new()
+	
+	data.set_type_node(type)
+	data.set_health(100)
+	data.set_global_position([10,20,30])
+	data.set_rotation([0,0,0])
+	netHandler.set_data(data)
+	
+	
+func decode_player_data():
+	for item_key in netHandler.players_net.keys():
+		player_data[item_key] = _decode_player(netHandler.players_net[item_key])
+	
+func _decode_player(bytes):
+	var player = MoonProto.PlayerInfo.new()
+	var state = player.from_bytes(bytes)
+	
+	# Debug check https://github.com/oniksan/godobuf Unpack result code
+	print(state)
+	if state == MoonProto.PB_ERR.NO_ERRORS:
+		print("Data received")
+	
+	print(player.get_health())
+	print(player.get_global_position())
+	print(player.get_rotation())
+	
+	
+	return 0
--- Razor/client/network/net_v2.gd
+++ Razor/client/network/net_v2.gd
@@ -8,6 +8,16 @@ var CLIENT_PORT: int = 60000
 var P2P_PORT: int =  31400
 var MAX_CLIENTS: int = 32
 
+var players_net = {}
+var client_data = null
+
+func _ready():
+	get_tree().connect("network_peer_connected", self, "_player_connected")
+	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
+	get_tree().connect("connected_to_server", self, "_connected_ok")
+	get_tree().connect("connection_failed", self, "_connected_fail")
+	get_tree().connect("server_disconnected", self, "_server_disconnected")
+
 func create_server():
 	var peer = NetworkedMultiplayerENet.new()
 	peer.create_server(SERVER_PORT, MAX_CLIENTS)
@@ -22,3 +32,29 @@ func connect_client():
 	peer.create_client(SERVER_IP, SERVER_PORT)
 	get_tree().network_peer = peer
 
+func refuse_new_connection():
+	get_tree().network_peer.refuse_new_connections()
+
+func _player_connected(id):
+	# Called on both clients and server when a peer connects. Send my info to it.
+	rpc_id(id, "register_player", client_data)
+
+func _player_disconnected(id):
+	players_net.erase(id)
+
+func _connected_ok():
+	pass # Only called on clients, not server. Will go unused; not useful here.
+
+func _server_disconnected():
+	pass # Server kicked us; show error and abort.
+
+func _connected_fail():
+	pass # Could not even connect to server; abort.
+
+func set_data(data):
+	client_data = data
+
+# This will be executed in the server
+remote func register_player(data):	
+	var id = get_tree().get_rpc_sender_id()
+	players_net[id] = data
