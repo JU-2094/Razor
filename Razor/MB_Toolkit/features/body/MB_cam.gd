97c81759aea841ea03ce76a99ee2234e0a7af6dc
JU-2094 <urbgon@gmail.com>
2022-03-29 21:57:43 -0600

    fix camera problem with new toolkit

--- Razor/MB_Toolkit/features/body/MB_cam.gd
+++ Razor/MB_Toolkit/features/body/MB_cam.gd
@@ -1,16 +1,21 @@
 extends Position3D
 
 export var distance: float = 15.0
+export var height_from_floor: float = 20.0
+export(NodePath) var target_path
 
 func _process(delta):
-	var target: Vector3 = get_parent() \
-		.global_transform.origin
+	# Position of the object to follow
+	var target: Vector3 = get_node(target_path).get_node("Body").global_transform.origin
+	
+	# Relative position to the world
 	var pos: Vector3 = global_transform.origin
 	
-	var new_pos: Vector3 = Vector3(
-		pos.x - target.x , 0, pos.z - target.z - distance).normalized()
+	print(pos)
+	print(target)
 	
+	pos.y = height_from_floor
 	pos.x = target.x
-	pos.z = target.z + distance
+	pos.z = target.z - distance
 	
 	global_transform.origin = pos
