5acb24f36be8b883e811f29bf5a2f1855714fa4b
JU-2094 <urbgon@gmail.com>
2022-10-08 00:14:21 -0500

    camera circle v1

--- Razor/MB_Toolkit/features/body/MB_cam_circle.gd
+++ Razor/MB_Toolkit/features/body/MB_cam_circle.gd
@@ -2,6 +2,7 @@ extends Position3D
 
 export var distance: float = 1.0
 export var height_from_floor: float = 1.0
+export var theta: float = 180
 export(NodePath) var target_path
 var init_origin: Vector3 = Vector3.ZERO
 
@@ -13,14 +14,15 @@ func _process(delta):
 	# Position of the object to follow with local coordinates
 	# The global coordinates are not updating when moving
 	var target: Vector3 = get_node(target_path).get_node("Body").global_translation
-	
-	# look_at(Vector3(target.x, 0, target.z), Vector3.UP)
-	
-	# translation.x = init_origin.x + target.x
-	# translation.z = init_origin.z + target.z - distance
+
+	look_at(Vector3(target.x, 0, target.z), Vector3.UP)
+	translation.x = target.x + distance*sin(deg2rad(theta))
+	translation.z = target.z + distance*cos(deg2rad(theta))
 	
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		if event.button_mask == BUTTON_MASK_LEFT:
-			print('left click , relative pos=', event.relative)
-	
+			if event.relative.x > 0:
+				theta = theta + 2
+			elif event.relative.x < 0:
+				theta = theta - 2
